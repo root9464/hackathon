d816c47fc2164a09c61aea150520a9fe449d2362
root9464 <senpai9464@yandex.ru>
2023-10-28 01:10:42 +0500

    28.10.2023::1:00

---
+++ components/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import { Route, NavLink } from "./route"
+import Image from "next/image"
+import logo from "@/img/logo.png"
+const Header = () => {
+    const NavItem:NavLink[] = [
+        {label: 'Возможности', href: '/'},
+        {label: 'Тарифы', href: '/about'},
+        {label: 'Внедрение', href: '/about'},
+        {label: 'Отзывы', href: '/about'},
+        {label: 'Поддержка', href: '/about'},
+    ]
+    return(
+        <header>
+            <div className="pos">
+                <Image  className="logo" src={logo} alt="logo"/>
+                <Route navLinks={NavItem}/>
+            </div>
+        </header>
+    )
+}
+export { Header }
---
+++ components/slider.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { motion } from "framer-motion"
+import image from '@/img/test.jpg'
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
+const Slider = () => {
+    const array:number[] = [...Array(15)].map(e=>~~(Math.random()*5))
+    const [width, setWidth] =useState<number>(0)
+    const corousel = useRef<any>()
+    useEffect(() => {
+        setWidth(corousel.current.scrollWidth - corousel.current.offsetWidth )  
+    },[])
+    
+    return (
+        <motion.div className="carousel" id="target1" ref={corousel} whileTap={{cursor: "grabbing"}}>
+            <motion.div drag="x" dragConstraints={{right:0, left: -width}} className="inner-carousel">
+                {
+                array.map((e:any) => {
+                    return(
+                        <motion.div className="item" key={e}>
+                            <Image src={image} alt='slide'></Image>
+                        </motion.div>
+                    )})
+                }
+            </motion.div>
+        </motion.div>
+    )
+}
+export { Slider }
